992cb49ec1842a0e0758f9f75b925044c111ecdb
Lorenzo Carneli <lorenzo.carneli@vitry94.org>
2022-08-21 16:01:09 +0200

    feat: add logger module (#4)
    * feat: add logger module
    
    * chore: update env example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 
 import { Config as ConfigModule } from '@app/config';
+import { Logger as LoggerModule } from '@app/logger';
 
 @Module({
-	imports: [ConfigModule],
+	imports: [ConfigModule, LoggerModule],
 })
 export class AppModule {}
--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -8,4 +8,8 @@ export default registerAs('api', () => ({
 		origins: env.get('API_CORS_ORIGINS').default(['*']).asArray(),
 		headers: env.get('API_CORS_HEADERS').default(['*']).asArray(),
 	},
+	logs: {
+		combinedPath: env.get('API_LOGS_COMBINED_PATH').default('logs/combined.log').asString(),
+		errorPath: env.get('API_LOGS_ERROR_PATH').default('logs/error.log').asString(),
+	},
 }));
