01bb9ec231daefaab8d5dad3324596bb4cebb4a5
Lorenzo Carneli <lorenzo.carneli@vitry94.org>
2022-08-23 02:26:19 +0200

    refactor: rewrite config module (#7)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 
-import { Config as ConfigModule } from '@app/config';
+import { ConfigModule } from '@app/config';
 import { Logger as LoggerModule } from '@app/logger';
 
 @Module({
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,2 @@
-export * from '@nestjs/config';
-
 export * from './api.config';
 export * from './config.module';
