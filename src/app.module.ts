10fd38a577ceccf2b4bfeabbe79601ae667897ec
Lorenzo Carneli <lorenzo.carneli@vitry94.org>
2022-08-28 18:52:31 +0200

    feat: add session module (#13)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { ConfigModule } from '@app/config';
 import { LoggerModule } from '@app/logger';
 import { PostgresModule } from '@app/postgres';
 import { RedisModule } from '@app/redis';
+import { SessionModule } from '@app/session';
 import { UsersModule } from '@app/users';
 
 @Module({
-	imports: [ConfigModule, LoggerModule, PostgresModule, RedisModule, UsersModule],
+	imports: [ConfigModule, LoggerModule, PostgresModule, RedisModule, SessionModule, UsersModule],
 })
 export class AppModule {}
