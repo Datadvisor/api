c0c1bdbf4b38463592b676c5dd8fff5036117b8f
Lorenzo Carneli <lorenzo.carneli@vitry94.org>
2022-09-01 01:32:16 +0200

    feat: add user controller

---
+++ src/users/user.controller.ts
@@ -0,0 +1,79 @@
+import {
+	Body,
+	ClassSerializerInterceptor,
+	ConflictException,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	HttpStatus,
+	Patch,
+	UseInterceptors,
+} from '@nestjs/common';
+import {
+	ApiBadRequestResponse,
+	ApiConflictResponse,
+	ApiInternalServerErrorResponse,
+	ApiNoContentResponse,
+	ApiOkResponse,
+	ApiOperation,
+	ApiTags,
+	ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+
+import { UsersService } from './users.service';
+import { AuthUser } from '../auth/decorators';
+import { CurrentUser } from './decorators';
+import { User } from './entities';
+import { UserRo } from './ro';
+import { UpdateUserDto } from './dto';
+import { UserConflictException } from './exceptions';
+
+@ApiTags('users')
+@Controller('user')
+@UseInterceptors(ClassSerializerInterceptor)
+export class UserController {
+	constructor(private readonly usersService: UsersService) {}
+
+	@ApiOperation({ summary: 'Get current user' })
+	@ApiOkResponse({ description: 'Success', type: UserRo })
+	@ApiUnauthorizedResponse({ description: 'Unauthorized' })
+	@ApiInternalServerErrorResponse({ description: 'Internal server error' })
+	@Get()
+	@AuthUser()
+	@HttpCode(HttpStatus.OK)
+	async get(@CurrentUser() user: User): Promise<UserRo> {
+		return new UserRo(user);
+	}
+
+	@ApiOperation({ summary: 'Update current user' })
+	@ApiOkResponse({ description: 'Success', type: UserRo })
+	@ApiBadRequestResponse({ description: 'Bad request' })
+	@ApiUnauthorizedResponse({ description: 'Unauthorized' })
+	@ApiConflictResponse({ description: 'Conflict' })
+	@ApiInternalServerErrorResponse({ description: 'Internal server error' })
+	@Patch()
+	@AuthUser()
+	@HttpCode(HttpStatus.OK)
+	async update(@CurrentUser() user: User, @Body() payload: UpdateUserDto): Promise<UserRo | null> {
+		try {
+			return new UserRo(await this.usersService.update(user.id, payload));
+		} catch (err) {
+			if (err instanceof UserConflictException) {
+				throw new ConflictException(err.message);
+			}
+			throw err;
+		}
+	}
+
+	@ApiOperation({ summary: 'Delete current user' })
+	@ApiNoContentResponse({ description: 'Success' })
+	@ApiUnauthorizedResponse({ description: 'Unauthorized' })
+	@ApiInternalServerErrorResponse({ description: 'Internal server error' })
+	@Delete()
+	@AuthUser()
+	@HttpCode(HttpStatus.NO_CONTENT)
+	async delete(@CurrentUser() user: User): Promise<void> {
+		await this.usersService.delete(user.id);
+	}
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import { PostgresModule } from '../postgres';
+import { UserController } from './user.controller';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
 	imports: [PostgresModule],
-	controllers: [UsersController],
+	controllers: [UserController, UsersController],
 	providers: [UsersService],
 	exports: [UsersService],
 })
