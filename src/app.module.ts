2ca3641cee63bd4ad15127dc18a8379c590ab70e
Lorenzo Carneli <lorenzo.carneli@vitry94.org>
2022-09-01 18:20:43 +0200

    feat: add email module (#17)
    * feat: add email module
    
    * refactor: rename email send exception file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { AuthModule } from './auth';
 import { ConfigModule } from './config';
+import { EmailModule } from './email';
 import { LoggerModule } from './logger';
 import { PostgresModule } from './postgres';
 import { RedisModule } from './redis';
@@ -9,6 +10,15 @@ import { SessionModule } from './session';
 import { UsersModule } from './users';
 
 @Module({
-	imports: [AuthModule, ConfigModule, LoggerModule, PostgresModule, RedisModule, SessionModule, UsersModule],
+	imports: [
+		AuthModule,
+		ConfigModule,
+		EmailModule,
+		LoggerModule,
+		PostgresModule,
+		RedisModule,
+		SessionModule,
+		UsersModule,
+	],
 })
 export class AppModule {}
