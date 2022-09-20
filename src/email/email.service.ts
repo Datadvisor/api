7ba3138b5f38926880b2a45b83f747b1239de25e
Lorenzo Carneli <lorenzo.carneli@vitry94.org>
2022-09-20 20:58:12 +0200

    feat: add sandbox mode for the email service

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import * as SendGrid from '@sendgrid/mail';
+import * as sendGrid from '@sendgrid/mail';
 
 import { SendEmailDto } from './dto';
 
 @Injectable()
 export class EmailService {
 	constructor(private readonly configService: ConfigService) {
-		SendGrid.setApiKey(this.configService.get<string>('sendgrid.apiKey'));
+		sendGrid.setApiKey(this.configService.get<string>('sendgrid.apiKey'));
 	}
 
 	async send(payload: SendEmailDto): Promise<void> {
-		await SendGrid.send(payload);
+		await sendGrid.send({
+			...payload,
+			mailSettings: {
+				sandboxMode: {
+					enable: this.configService.get<string>('api.env') === 'test',
+				},
+			},
+		});
 	}
 }
