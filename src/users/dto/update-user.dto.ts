eb2057f7fef379118275e86974b8d563b0701d02
Lorenzo Carneli <lorenzo.carneli@vitry94.org>
2022-12-16 14:22:46 +0100

    refactor: updated user dto to allow user to change first name and last name

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -2,6 +2,4 @@ import { OmitType, PartialType } from '@nestjs/swagger';
 
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(
-	OmitType(CreateUserDto, ['lastName', 'firstName', 'newsletter'] as const),
-) {}
+export class UpdateUserDto extends PartialType(OmitType(CreateUserDto, ['newsletter'] as const)) {}
