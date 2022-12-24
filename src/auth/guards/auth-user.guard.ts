e6e8dbd0fe6ec9d16fd8a687d9aca7450465b4bf
Lorenzo Carneli <lorenzo.carneli@vitry94.org>
2022-12-24 15:46:17 +0100

    refactor: rewrite auth guards and some changes in users module

--- src/auth/guards/auth-user.guard.ts
+++ src/auth/guards/auth-user.guard.ts
@@ -42,6 +42,12 @@ export class AuthUserGuard implements CanActivate {
 			throw err;
 		}
 
+		const emailVerified = this.reflector.get<boolean>('emailVerified', ctx.getHandler());
+
+		if (emailVerified && !req.user.emailVerified) {
+			throw new ForbiddenException("You don't have permission to access to this resource");
+		}
+
 		const roles = this.reflector.get<Role[]>('roles', ctx.getHandler());
 
 		if (roles && roles.length && !AuthUserGuard.checkPermission(roles, req.user.role)) {
