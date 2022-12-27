25e9136bac8df342783abf1819cf6e8b9a67b922
Anthoni Marie <contact@anthoni-marie.fr>
2022-12-26 20:00:56 -0700

    fix: fixing eslint errors

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,14 +1,4 @@
-import * as mailchimp from '@mailchimp/mailchimp_marketing';
-import { AddListMemberBody } from '@mailchimp/mailchimp_marketing';
-import { Get, HttpCode, HttpStatus, Injectable, Post, UnprocessableEntityException } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import {
-	ApiBadRequestResponse,
-	ApiInternalServerErrorResponse,
-	ApiNoContentResponse,
-	ApiOperation,
-} from '@nestjs/swagger';
-import { Stripe } from 'stripe';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class PaymentService {}
